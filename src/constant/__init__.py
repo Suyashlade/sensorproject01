e93da9b1be13cb83b89dd851035ac32bf011442e
suyash lade <suyashclade@gmail.com>
2025-01-10 15:14:19 +0530

    completed all modules

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -5,7 +5,9 @@ MONGO_DATABASE_NAME="pwskills"
 MONGO_COLLECTION_NAME="waferfault"
 
 TARGET_COLUMN="quality"
-MONGO_DB_URL="mongodb+srv://snshrivas:Snshrivas@cluster0.u46c4.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DB_URL="mongodb+srv://suyashpw:9307371441@cluster0.x4xpc.mongodb.net/?retryWrites=true&w=majority"
 
 MODEL_FILE_NAME="model"
 MODEL_FILE_EXTENSION=".pkl"
+
+artifact_folder = "artifacts"
